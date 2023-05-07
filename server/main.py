bedc43b6ed0858679b30dddaf48a2a68ae01568f
Greg Garcia <greg@garcia.casa>
2023-05-07 14:08:59 -0400

    Fix: port number

--- server/main.py
+++ server/main.py
@@ -107,4 +107,4 @@ async def logo_png():
         raise HTTPException(status_code=404, detail="Logo file not found.")
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=8080, reload=True)
