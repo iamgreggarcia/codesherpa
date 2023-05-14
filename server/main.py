4f26405081bc7556707c5f32e7a63b5e65886009
Greg Garcia <greg@garcia.casa>
2023-05-14 19:31:46 -0400

    Add: google cloun run service

--- server/main.py
+++ server/main.py
@@ -15,10 +15,12 @@ from contextlib import redirect_stderr, redirect_stdout
 import uvicorn
 from models.api import CodeExecutionRequest, CommandExecutionRequest
 from loguru import logger
-from .container_manager import ContainerManager
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import httpx
 
 logger.remove()
 logger.add(sys.stderr, level="INFO")
@@ -28,7 +30,6 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-container_manager = ContainerManager()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="well-known")
@@ -38,36 +39,28 @@ persistent_console = code.InteractiveConsole()
 @app.post("/repl")
 @limiter.limit("5/minute")
 async def repl(request: Request, code_execution_request: CodeExecutionRequest):
-    code_output = execute_code_in_repl(code_execution_request.code)
-    return {"result": code_output}
+    response = await send_request_to_gcloud_run(code_execution_request)
+    return response
 
-def execute_code_in_repl(code_list: List[str]) -> str:
-    output = io.StringIO()
+async def send_request_to_gcloud_run(code_execution_request: CodeExecutionRequest):
+    url = os.environ.get('GOOGLE_CLOUD_RUN_URL')
 
-    try:
-        with redirect_stdout(output), redirect_stderr(output):
-            for code_line in code_list:
-                for command in code_line.split(';'):
-                    result = persistent_console.push(command.strip())
-        result = output.getvalue()
+    # Load the service account key from the environment variable
+    service_account_key = json.loads(os.environ.get('GOOGLE_SERVICE_ACCOUNT_KEY'))
 
-    except Exception as e:
-        result = str(e)
+    # Use the key to create credentials
+    credentials = service_account.Credentials.from_service_account_info(service_account_key)
 
-    return result
+    # Create an access token
+    auth_req = google.auth.transport.requests.Request()
+    credentials.refresh(auth_req)
+    token = credentials.token
 
-async def execute_command(command: str, container) -> str:
-    try:
-        result = container.exec_run(command, stream=True)
-        output = ""
-        for line in result[1]:
-            if isinstance(line, bytes):
-                output += line.decode("utf-8")
-            else:
-                output += str(line)
-        return f"Command execution result:\n{output}"
-    except Exception as e:
-        return f"Error executing command: {str(e)}"
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    data = {"code": code_execution_request.code}
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=data)
+    return response.json()
 
 @app.get("/openapi.yaml")
 async def openapi_yaml():
