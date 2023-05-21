c430efd9982914eec10fedb6926b7633af278461
Greg Garcia <greg@garcia.casa>
2023-05-21 12:46:50 -0400

    Update .well-known manifest and openapi spec

---
+++ server/config.py
@@ -0,0 +1,9 @@
+"""
+Configuration settings for the server module.
+
+This file contains the API version and other configuration settings
+specific to the server module. You can update the API version here,
+and it will be reflected across the server module.
+"""
+
+API_VERSION = "/api/v1"
