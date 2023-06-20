05a45b38a74786dcd6d661cd728656edb7fae85b
Greg Garcia <greg@garcia.casa>
2023-06-20 10:47:46 -0400

    Feat: declare request example data

--- models/api.py
+++ models/api.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel, Field
 
 class CodeExecutionRequest(BaseModel):
+    """
+    A Pydantic model representing a request to execute code.
+
+    Attributes:
+        language (str): The programming language of the code to be executed.
+        code (str): The code to be executed.
+    """
     language: str = Field(..., example="python")
     code: str = Field(..., example="print('Hello, World!')")
 
@@ -13,6 +20,12 @@ class CodeExecutionRequest(BaseModel):
         }
 
 class CommandExecutionRequest(BaseModel):
+    """
+    A Pydantic model representing a request to execute a command.
+
+    Attributes:
+        command (str): The command to be executed.
+    """
     command: str = Field(..., example="ls -la")
 
     class Config:
