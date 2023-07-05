d6b453aa1fc9745bf788397fcdd1464ed42605ea
Greg Garcia <greg@garcia.casa>
2023-07-05 15:43:28 -0400

    Update: tests error handling

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -9,15 +9,16 @@ def test_python_executor():
     result = executor.execute(code)
     assert result == "50\n"
 
-    # Negative Case
+    # Negative Caseb
     code = "print('Hello, World!'"
     with pytest.raises(SyntaxError) as excinfo:
         result = executor.execute(code)
     assert "'(' was never closed" in str(excinfo.value)
 
-# def test_tear_down():
-#     print("Tearing down...")
-#     file_names = ["script.cpp", "script.rs", "script", "a.out"]
-#     for file_name in file_names:
-#         file_path = os.path.join(os.getcwd(), file_name)
-#         os.remove(file_path)
+def test_tear_down():
+    print("Tearing down...")
+    file_names = ["script.cpp", "script.rs", "script", "a.out"]
+    if file_names:
+        for file_name in file_names:
+            file_path = os.path.join(os.getcwd(), file_name)
+            os.remove(file_path)
