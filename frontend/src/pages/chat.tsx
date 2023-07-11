986730397c6ef05018c869015c1bfc3d9339f6c8
Greg Garcia <greg@garcia.casa>
2023-07-11 19:40:55 -0400

    Update: model-selector

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -318,140 +318,137 @@ export default function Chat() {
   }, [messages, textareaRef.current]);
 
   return (
-    <>
-      <div className="relative h-screen mx-0">
-        <div className="flex flex-col h-screen p-6 mx-14">
-          <div className={`absolute top-0 left-0 w-full border-transparent dark:border-white/20 dark:via-[#343541] dark:to-[#343541] 
+
+    <div className="relative h-screen mx-0">
+      <div className="flex flex-col h-screen p-6 mx-14">
+        <div className={`absolute top-0 left-0 w-full border-transparent dark:border-white/20 dark:via-[#343541] dark:to-[#343541] 
         ${conversationStarted ? 'pt-0 md:pt-0' : 'pt-8 md:pt-6'}`}>
-            <div className={`flex flex-row justify-center z-50 items-center pt-0 mx-0 md:mx-0 ${conversationStarted ? 'fixed' : ''}`}>
-              <ModelSelector selectedModel={selectedModel} setSelectedModel={setSelectedModel} conversationStarted={conversationStarted} />
-            </div>
-            <div className="mx-2 mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto">
-              <div className="flex-1 overflow-auto mt-12 mb-40 bg-transparent">
-                {messages.map((message, index) => message.role !== 'system' && <ChatMessage key={index} message={message} isStreaming={messageIsStreaming} streamingMessageIndex={messages.length - 1} currentMessageIndex={index} isFunctionCall={isFunctionCall} selectedModel={selectedModel} />)}
-                <div ref={messageEndRef} />
-              </div>
+          <div className={`flex flex-row justify-center z-50 items-center pt-0 mx-0 md:mx-0 ${conversationStarted ? 'fixed' : ''}`}>
+            <ModelSelector selectedModel={selectedModel} setSelectedModel={setSelectedModel} conversationStarted={conversationStarted} />
+          </div>
+          <div className="mx-2 mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto">
+            <div className="flex-1 overflow-auto mt-12 mb-40 bg-transparent">
+              {messages.map((message, index) => message.role !== 'system' && <ChatMessage key={index} message={message} isStreaming={messageIsStreaming} streamingMessageIndex={messages.length - 1} currentMessageIndex={index} isFunctionCall={isFunctionCall} selectedModel={selectedModel} />)}
+              <div ref={messageEndRef} />
             </div>
           </div>
-          <div className="fixed border-0 bottom-0 left-0 w-full dark:border-orange-200 bg-gradient-to-b from-transparent via-white to-white pt-6 dark:via-[#1f232a] dark:to-[#1f232a] md:pt-2">
-            <div className="stretch mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto lg:max-w-3xl">
-              <div className="relative flex h-full flex-1 items-stretch md:flex-col" role="presentation">
-                <div className="h-full flex ml-1 md:w-full md:m-auto md:mb-2 gap-0 md:gap-2 justify-center">
-                  {messageIsStreaming ? (
-                    <button
-                      className="btn relative btn-neutral -z-0 border-0 md:border"
-                      onClick={stopConversationHandler}
-                    >
-                      <div className="flex w-full gap-2 items-center justify-center">
-                        {/* ... SVG for Stop Generating ... */}
-                        Stop Generating
-                      </div>
-                    </button>
-                  ) : conversationStarted ? (
-                    <button
-                      className="btn relative btn-neutral -z-0 border-0 md:border"
-                      onClick={regenerateResponseHandler}
-                    >
-                      <div className="flex w-full gap-2 items-center justify-center">
-                        {/* ... SVG for Regenerate Response ... */}
-                        Regenerate Response
-                      </div>
-                    </button>
-                  ) : null}
-                </div>
-
-                <div className="relative flex mx-1 flex-col h-full flex-1 items-stretch border-black/10 bg-slate-100 shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:bg-gray-700 dark:text-white dark:focus:border-12 dark:shadow-[0_0_20px_rgba(0,0,0,0.10)] sm:mx-4 rounded-xl dark:outline-none outline-none">
-
-                  {uploadedFileName &&
-                    <div className="md:mx-2 mt-2">
-                      <UploadedFile filename={uploadedFileName} onDelete={onDeleteFile} />
-                    </div>
-                  }
-                  <textarea
-                    ref={textareaRef}
-                    className="flex-grow outline-none m-0 w-full dark:border-none border border-black resize-none bg-transparent py-4 pl-96  text-black dark:bg-transparent dark:text-white md:pl-[30px] rounded-md placeholder:text-gray-400 dark:placeholder:text-gray-300"
-                    style={{
-                      maxHeight: '400px',
-                      overflow: `${textareaRef.current && textareaRef.current.scrollHeight > 400 ? 'auto' : 'hidden'}`,
-                      minHeight: '56px',
-                      cursor: 'text',
-                      paddingLeft: `${selectedModel === Model.GPT3_5_CODE_INTERPRETER_16K || selectedModel === Model.GPT4_CODE_INTERPRETER ? '50px' : '30px'}`,
-                      paddingRight: '34px',
-
-                    }}
-                    placeholder={`Send a message`}
-                    value={newMessage}
-                    rows={1}
-                    onCompositionStart={() => setIsTyping(true)}
-                    onCompositionEnd={() => setIsTyping(false)}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter' && !isTyping && !isMobile() && !e.shiftKey) {
-                        e.preventDefault();
-                        handleSendMessage(e);
-                      }
-                    }} />
-
-
-                  <input
-                    type="file"
-                    id="fileUpload"
-                    className="hidden"
-                    onChange={onUploadFile}
-                  />
-                  {(selectedModel === Model.GPT3_5_CODE_INTERPRETER_16K || selectedModel === Model.GPT4_CODE_INTERPRETER) &&
-                    <button
-                      className="absolute left-4 bottom-4 p-0 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:hover:bg-opacity-20 dark:hover:bg-neutral-200 dark:hover:rounded-full transition-all duration-200 dark:text-neutral-100 dark:hover:text-neutral-100"
-                      onClick={() => document.getElementById('fileUpload')?.click()}
-                      onKeyDown={() => { }}
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      </svg>
-                    </button>}
+        </div>
+        <div className="fixed border-0 bottom-0 left-0 w-full dark:border-orange-200 bg-gradient-to-b from-transparent via-white to-white pt-6 dark:via-[#1f232a] dark:to-[#1f232a] md:pt-2">
+          <div className="stretch mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto lg:max-w-3xl">
+            <div className="relative flex h-full flex-1 items-stretch md:flex-col" role="presentation">
+              <div className="h-full flex ml-1 md:w-full md:m-auto md:mb-2 gap-0 md:gap-2 justify-center">
+                {messageIsStreaming ? (
                   <button
-                    type="submit"
-                    className={`absolute right-3 bottom-4 p-1 rounded-md text-neutral-800 opacity-90 ${messageIsStreaming ? 'bg-red-500' : newMessage.length === 0 ? 'bg-transparent text-neutral-800 opacity-60' : 'bg-fuchsia-500 text-neutral-100'} dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-900 duration-100 transition-all`}
-                    onClick={messageIsStreaming ? stopConversationHandler : handleSendMessage}
-                    disabled={newMessage.length === 0 && !messageIsStreaming}
+                    className="hidden md:block btn relative btn-neutral -z-0 border-0 md:border"
+                    onClick={stopConversationHandler}
                   >
-                    {messageIsStreaming ? (
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path className="animate-pulse duration-150" strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
-                      </svg>
-                    ) : (
-                      <PaperAirplaneIcon className={`duration-100 text-slate-100 transition-all ${newMessage.length === 0 && !fileIsAttached ? 'h-0 w-0' : 'h-5 w-5'}`} />
-                    )}
+                    <div className="flex w-full gap-2 items-center justify-center">
+                      {/* ... SVG for Stop Generating ... */}
+                      Stop Generating
+                    </div>
                   </button>
-                  {/* <button
+                ) : conversationStarted ? (
+                  <button
+                    className="hidden md:block btn relative btn-neutral -z-0 border-0 md:border"
+                    onClick={regenerateResponseHandler}
+                  >
+                    <div className="flex w-full gap-2 items-center justify-center">
+                      {/* ... SVG for Regenerate Response ... */}
+                      Regenerate Response
+                    </div>
+                  </button>
+                ) : null}
+              </div>
+
+              <div className=" relative flex mx-1 flex-col h-full flex-1 items-stretch border-black/10 bg-slate-100 shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:bg-gray-700 dark:text-white dark:focus:border-12 dark:shadow-[0_0_20px_rgba(0,0,0,0.10)] sm:mx-4 rounded-xl dark:outline-none outline-none">
+
+                {uploadedFileName &&
+                  <div className="md:mx-2 mt-2">
+                    <UploadedFile filename={uploadedFileName} onDelete={onDeleteFile} />
+                  </div>
+                }
+                <textarea
+                  ref={textareaRef}
+                  className="flex-grow outline-none m-0 w-full dark:border-none border border-black resize-none bg-transparent py-4 pl-96  text-black dark:bg-transparent dark:text-white md:pl-[30px] rounded-md placeholder:text-gray-400 dark:placeholder:text-gray-300"
+                  style={{
+                    maxHeight: '400px',
+                    overflow: `${textareaRef.current && textareaRef.current.scrollHeight > 400 ? 'auto' : 'hidden'}`,
+                    minHeight: '56px',
+                    cursor: 'text',
+                    paddingLeft: `${selectedModel === Model.GPT3_5_CODE_INTERPRETER_16K || selectedModel === Model.GPT4_CODE_INTERPRETER ? '50px' : '30px'}`,
+                    paddingRight: '34px',
+
+                  }}
+                  placeholder={`Send a message`}
+                  value={newMessage}
+                  rows={1}
+                  onCompositionStart={() => setIsTyping(true)}
+                  onCompositionEnd={() => setIsTyping(false)}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter' && !isTyping && !isMobile() && !e.shiftKey) {
+                      e.preventDefault();
+                      handleSendMessage(e);
+                    }
+                  }} />
+
+
+                <input
+                  type="file"
+                  id="fileUpload"
+                  className="hidden"
+                  onChange={onUploadFile}
+                />
+                {(selectedModel === Model.GPT3_5_CODE_INTERPRETER_16K || selectedModel === Model.GPT4_CODE_INTERPRETER) &&
+                  <button
+                    className="absolute left-4 bottom-4 p-0 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:hover:bg-opacity-20 dark:hover:bg-neutral-200 dark:hover:rounded-full transition-all duration-200 dark:text-neutral-100 dark:hover:text-neutral-100"
+                    onClick={() => document.getElementById('fileUpload')?.click()}
+                    onKeyDown={() => { }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </button>}
+                <button
+                  type="submit"
+                  className={`absolute right-3 bottom-4 p-1 rounded-md text-neutral-800 opacity-90 ${newMessage.length === 0 ? 'bg-transparent text-neutral-800 opacity-60' : 'bg-fuchsia-500 text-neutral-100'} dark:bg-opacity-50 dark:text-neutral-100 dark:hover:text-neutral-900 duration-100 transition-all`}
+                  onClick={handleSendMessage}
+                  disabled={newMessage.length === 0 || messageIsStreaming}
+                  hidden={messageIsStreaming}
+                >
+                  <PaperAirplaneIcon className={`duration-100 text-slate-100 transition-all ${newMessage.length === 0 && !fileIsAttached ? 'h-0 w-0' : 'h-5 w-5'}`} />
+                </button>
+                {!messageIsStreaming && <span className="absolute right-3 bottom-4 loading loading-bars loading-md text-fuchsia-600"></span>
+                }
+                {/* <button
                   className="absolute left-4 bottom-2 p-0 text-neutral-800 opacity-60 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-opacity-50 dark:hover:bg-opacity-20 dark:hover:bg-neutral-200 dark:hover:rounded-full transition-all duration-200 dark:text-neutral-100 dark:hover:text-neutral-100"
                   onClick={}
                   onKeyDown={}
                 > */}
-                  {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                   </svg> */}
-                  {/* </button> */}
-                </div>
-
-
-
-
+                {/* </button> */}
               </div>
-              <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">            <a
-                href="https://github.com/iamgreggarcia/codesherpa"
-                target="_blank"
-                rel="noreferrer"
-                className="underline"
-              >
-              </a>
-                {' '}
 
-              </div>
+
+
+
+            </div>
+            <div className="hidden md:block px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">            <a
+              href="https://github.com/iamgreggarcia/codesherpa"
+              target="_blank"
+              rel="noreferrer"
+              className="underline"
+            >
+            </a>
+              {' '}
+
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
+
   );
 }
