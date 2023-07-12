d1d1ff364073b098fc514cce2f760fe3d1c88e41
Greg Garcia <greg@garcia.casa>
2023-07-11 21:42:23 -0400

    Update: stop gen svg

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -342,7 +342,9 @@ export default function Chat() {
                     onClick={stopConversationHandler}
                   >
                     <div className="flex w-full gap-2 items-center justify-center">
-                      {/* ... SVG for Stop Generating ... */}
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
+                      </svg>
                       Stop Generating
                     </div>
                   </button>
